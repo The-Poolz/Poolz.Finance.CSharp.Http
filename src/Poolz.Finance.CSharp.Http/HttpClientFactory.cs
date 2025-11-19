2c5b82d9a22984bdd884dddc13c719b6b99a7d85
ArdenHide <ardenhide@gmail.com>
2025-11-19 16:45:22 +0300

    init

---
+++ src/Poolz.Finance.CSharp.Http/HttpClientFactory.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Http.Headers;
+
+namespace Poolz.Finance.CSharp.Http;
+
+public class HttpClientFactory : IHttpClientFactory
+{
+    public HttpClient Create(string url, Action<HttpRequestHeaders>? configureHeaders = null)
+    {
+        var client = new HttpClient(new FailureOnlyLoggingHandler(new HttpClientHandler()))
+        {
+            BaseAddress = new Uri(url)
+        };
+
+        configureHeaders?.Invoke(client.DefaultRequestHeaders);
+        return client;
+    }
+}
---
+++ src/Poolz.Finance.CSharp.Http/IHttpClientFactory.cs
@@ -0,0 +1,8 @@
+﻿using System.Net.Http.Headers;
+
+namespace Poolz.Finance.CSharp.Http;
+
+public interface IHttpClientFactory
+{
+    public HttpClient Create(string url, Action<HttpRequestHeaders>? configure = null);
+}
